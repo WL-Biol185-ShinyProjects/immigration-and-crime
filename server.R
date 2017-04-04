495c5632c1b45c655c5043c4bd59cca84e673e49
Harrison Westgarth <westgarthh17@mail.wlu.edu>
2017-04-03 20:28:28 -0400

    Fixed app probs, added additional year filter, looking into formatting tabs and various graphs

--- server.R
+++ server.R
@@ -4,12 +4,14 @@ library(ggplot2)
 shinyServer(function(input, output) {
 master_immigration <- read.csv("Master_Immigration.csv") 
 crime <- read.csv("CRIME.csv")
+rate <- read.csv("rate.csv")
 
   output$immtypePlot <- renderPlot({
     master_immigration%>%
       filter(immigrant_type == input$immigrant_type) %>%
       filter(country == input$country) %>%
-      ggplot(aes(year, number, color = country)) + geom_line()
+      filter(year >= input$year[1], year <= input$year[2]) %>%
+      ggplot(aes(year, number, color = country)) + geom_point() + geom_smooth(method = "loess")
   })
     
 
@@ -19,12 +21,10 @@ crime <- read.csv("CRIME.csv")
       ggplot(aes(year, crime_rate, color = type)) + geom_line()
   })
   
-  
-  #output$totalrate <- renderPlot({
-  #rate %>%
-    #filter(country == input$country) %>%
-    #ggplot(aes(year, number, color = country)) + geom_line()
-  #})
-  
+  output$totalratePlot <- renderPlot({
+    rate %>%
+      filter(country == input$totalcountry) %>%
+      ggplot(aes(year, number, color = country)) + geom_line()
+  })
   
   })
--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 master_immigration <- read.csv("Master_Immigration.csv")
 crime <- read.csv("CRIME.csv")
+rate <- read.csv("rate.csv")
 # Define UI for application that draws a histogram
 fluidPage(
   
@@ -13,14 +14,16 @@ fluidPage(
 
         selectInput("immigrant_type", label = "Type", choices = unique(master_immigration$immigrant_type)),
         checkboxGroupInput("country", label = "Country", choices = unique(master_immigration$country)),
-        checkboxGroupInput("type", label = "Crime Type", choices = unique(crime$type))
+        sliderInput("year", label = "Time Period", min = 1800, max = 2017, value = c(1800,2017)),
+        checkboxGroupInput("type", label = "Crime Type", choices = unique(crime$type)),
+        checkboxGroupInput("totalcountry", label = "Country for Overall Rate", choices = unique(rate$country))
         ),
 
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("immtypePlot"),
-      plotOutput("crimetimePlot")
-      #plotOutput("totalrate")
+      plotOutput("crimetimePlot"),
+      plotOutput("totalratePlot")
   )
 
 
