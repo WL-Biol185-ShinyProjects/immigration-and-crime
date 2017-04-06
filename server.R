4fbdbcad0e5ade56fe09d0c843bbbc6ffc89aab9
Harrison Westgarth <westgarthh17@mail.wlu.edu>
2017-04-06 12:38:50 -0400

    Error in crime data, added year filter to crime

--- server.R
+++ server.R
@@ -6,10 +6,11 @@ master_immigration <- read.csv("Master_Immigration.csv")
 crime <- read.csv("CRIME.csv")
 rate <- read.csv("rate.csv")
 rate$X = NULL
+
   output$immtypePlot <- renderPlot({
     master_immigration%>%
       filter(immigrant_type == input$immigrant_type) %>%
-      filter(country == input$country) %>%
+      filter(country %in% input$country) %>%
       filter(year >= input$year[1], year <= input$year[2]) %>%
       ggplot(aes(year, number, color = country)) + geom_point() + geom_smooth(method = "loess", span =.3)
   })
@@ -17,40 +18,42 @@ rate$X = NULL
 
   output$crimetimePlot <- renderPlot({
     crime %>%
-      filter(type == input$type) %>%
+      filter(type %in% input$type) %>%
+      filter(year >= input$year[1], year <= input$year[2]) %>%
       ggplot(aes(year, crime_rate, color = type)) + geom_point() + geom_smooth(method = "loess", span =.3)
   })
   
   output$totalratePlot <- renderPlot({
     rate %>%
-      filter(country == input$totalcountry) %>%
+      filter(country %in% input$totalcountry) %>%
       filter(year >= input$year[1], year <= input$year[2]) %>%
       ggplot(aes(year, number, color = country)) + geom_point() + geom_smooth(method = "loess", span =.3)
   })
   
   output$regionPlot <- renderPlot({
     master_immigration %>%
-     filter(region == input$region) %>%
+     filter(region %in% input$region) %>%
      filter(year >= input$year[1], year <= input$year[2])%>%
      ggplot(aes(year, number, color = country)) + geom_point()
   })
   
   output$totalrateTable <- renderDataTable({
     rate %>%
-      filter(country == input$totalcountry) %>%
+      filter(country %in% input$totalcountry) %>%
       filter(year >= input$year[1], year <= input$year[2])
   })
   
 
   output$regionTable <- renderDataTable({
     master_immigration %>%
-      filter(region == input$region) %>%
+      filter(region %in% input$region) %>%
       filter(year >= input$year[1], year <= input$year[2])
   })
   
   output$crimetimeTable <- renderDataTable({
     crime %>%
-      filter(type == input$type)
+      filter(type %in% input$type) %>%
+      filter(year >= input$year[1], year <= input$year[2])
   })
   
   output$immtypeTable <- renderDataTable({
