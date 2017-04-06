85ff8022a94cb1753d0be63ad2a5fc02e512fc42
Kara Farroni <farronik17@mail.wlu.edu>
2017-04-06 12:00:19 -0400

    formatting

--- ui.R
+++ ui.R
@@ -14,26 +14,51 @@ fluidPage(
   sidebarLayout(
     sidebarPanel(
 
-        selectInput("immigrant_type", label = "Type", choices = unique(master_immigration$immigrant_type), selected = "Lawful"),
-        selectizeInput("country", label = "Country", choices = unique(master_immigration$country), selected = "Mexico", multiple = TRUE),
-        sliderInput("year", label = "Time Period", min = 1800, max = 2017, value = c(1800,2017)),
-        checkboxGroupInput("region", label = "Region", choices = unique(master_immigration$region), selected = "Middle East"),
-        checkboxGroupInput("type", label = "Crime Type", choices = unique(crime$type), selected = "Drug Related"),
-        selectizeInput("totalcountry", label = "Country for Overall Rate", choices = unique(rate$country), selected = "Mexico")
+        selectInput("immigrant_type", 
+                    label = "Type", 
+                    choices = unique(master_immigration$immigrant_type), 
+                    selected = "Lawful"),
+        selectizeInput("country", 
+                       label = "Country", 
+                       choices = unique(master_immigration$country), 
+                       selected = "Mexico", multiple = TRUE),
+        sliderInput("year", 
+                    label = "Time Period", 
+                    min = 1800, 
+                    max = 2017, 
+                    value = c(1800,2017)
+                    ),
+        checkboxGroupInput("region", 
+                           label = "Region", 
+                           choices = unique(master_immigration$region), 
+                           selected = "Middle East"),
+        checkboxGroupInput("type", 
+                           label = "Crime Type", 
+                           choices = unique(crime$type), 
+                           selected = "Drug Related"),
+        selectizeInput("totalcountry", 
+                       label = "Country for Overall Rate", 
+                       choices = unique(rate$country), 
+                       selected = "Mexico")
         ),
 
     # Show a plot of the generated distribution
     mainPanel(
       tabsetPanel(type="pills",
-                  tabPanel('Immigrant Type', plotOutput("immtypePlot")),
-                  tabPanel('Crime Rates', plotOutput("crimetimePlot")),
-                  tabPanel('Immigration Rates', plotOutput("totalratePlot")),
-                  tabPanel('Immigration by Region', plotOutput("regionPlot"))
+                  tabPanel('Immigrant Type', 
+                           plotOutput("immtypePlot")),
+                  tabPanel('Crime Rates', 
+                           plotOutput("crimetimePlot")),
+                  tabPanel('Immigration Rates', 
+                           plotOutput("totalratePlot")),
+                  tabPanel('Immigration by Region', 
+                           plotOutput("regionPlot"))
                   
                   
                   )
- 
+            )
 
 
-  )))
+        )
+  )
   
